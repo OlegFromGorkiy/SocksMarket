12f215c390fead6b5720800dd9999cd0400b8a5b
Oleg Kartashev <officinator@gmail.com>
2023-01-22 13:30:07 +0300

    Initial commit

---
+++ src/main/java/pro/sky/socksmarket/SocksMarketApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.socksmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksMarketApplication.class, args);
+    }
+
+}
